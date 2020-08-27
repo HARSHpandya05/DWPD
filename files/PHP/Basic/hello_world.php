abdaafe2e6fcbb0562666b2858d66a46f5c4931d
Kaushal Patel <55987371+mrkaushalpatel@users.noreply.github.com>
2020-08-27 11:59:47 +0530

    First Commit

---
+++ files/PHP/Basic/hello_world.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$VarA = 10;
+$VarB = 15;
+
+$VarC = $VarA + $VarB;
+
+// Print Style 1
+echo $VarC;
+// Print Style 2
+print $VarC;
+// Print Style 3
+print_r($VarC);
+?>
+</body>
+</html>
